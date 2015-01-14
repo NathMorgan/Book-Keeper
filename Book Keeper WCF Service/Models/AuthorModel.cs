a24191f554a2051a23c22c89b9e4fcedbb3ffb6e
Nathan <tuqiri@tuqiri.net>
2015-01-14 12:56:01 +0000

    Added Models

---
+++ Book Keeper WCF Service/Models/AuthorModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Book_Keeper_WCF_Service.Models
+{
+    public class AuthorModel
+    {
+    }
+}
