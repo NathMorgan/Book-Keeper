760a4ee97ab289ece42bfa6435971f1b12b42664
Nathan <tuqiri@tuqiri.net>
2015-01-19 04:52:45 +0000

    Added code for the button events

--- Book Keeper/MainWindow.xaml.cs
+++ Book Keeper/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using Book_Keeper.BookKeeperSR;
 using Book_Keeper.Classes;
+using Book_Keeper.Windows;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,40 +25,103 @@ namespace Book_Keeper
     public partial class MainWindow : Window
     {
         BookHandler bookhandle = new BookHandler();
+        ObservableCollection<string> bookNameList = new ObservableCollection<string>();
 
         public MainWindow()
         {
             InitializeComponent();
-        }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
+            TotalStockVal.Content = bookhandle.getTotalStock() + " (£" + bookhandle.getTotalStockPrice() + ")";
 
-        }
-
-        private void AddRecordButton()
-        {
-
+            var books = bookhandle.getBooks();
+            foreach (var book in books)
+            {
+                bookNameList.Add(book.Title);
+            }
+            BookListBox.ItemsSource = bookNameList;
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+        }
+
+        private void BookListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(BookListBox.SelectedIndex != -1)
+            {
+                var selectedBookName = BookListBox.SelectedValue.ToString();
+
+                var bookHander = new BookHandler();
+
+                List<BookModel> books = bookHander.getBooks();
+
+                BookModel selectedBook = books.Where(x => x.Title == selectedBookName).First();
+
+                if (selectedBook != null)
+                {
+                    AuthorVal.Text = "";
+
+                    foreach (AuthorModel author in selectedBook.Authors)
+                    {
+                        AuthorVal.Text += author.Name + "\n";
+                    }
+                    TitleVal.Text = selectedBook.Title;
+                    StockVal.Text = selectedBook.Stock.ToString();
+                    PriceVal.Text = "£" + selectedBook.Price;
+
+                    if (selectedBook.Description != "")
+                        DescriptionVal.Text = selectedBook.Description;
+                    if (selectedBook.Note != "")
+                        NoteVal.Text = selectedBook.Note;
+                }
+            }
+            else
+            {
+                TitleVal.Text = "N/A";
+                StockVal.Text = "N/A";
+                PriceVal.Text = "N/A";
+                DescriptionVal.Text = "N/A";
+                NoteVal.Text = "N/A";
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
             var bookHander = new BookHandler();
 
-            TotalStockVal.Content = bookHander.getTotalStock() + " (£" + bookhandle.getTotalStockPrice() + ")";
+            //Getting current button clicked
+            string buttonName = (sender as Button).Content.ToString();
 
-            List<BookModel> books = bookHander.getBooks().ToList();
+            //Outputting to console that the button was clicked
+            Console.WriteLine(DateTime.Now.ToString("h:mm:ss") + ": Button clicked: " + buttonName);
 
-            BookListBox.ItemsSource = books.Select(x => x.Title);
+            switch (buttonName)
+            {
+                case "Delete" :
+                    MessageBoxResult deleteResult = MessageBox.Show("Do you wish to delete this book?", "Confirm Delete", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                    if (deleteResult == MessageBoxResult.Yes)
+                    {
+                        bookhandle.DeleteBookByName(BookListBox.SelectedItem.ToString());
+                        TotalStockVal.Content = bookHander.getTotalStock() + " (£" + bookhandle.getTotalStockPrice() + ")";
 
+                        bookNameList.RemoveAt(BookListBox.SelectedIndex);
 
-            /*
-            System.Windows.Data.CollectionViewSource bookModelViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("bookModelViewSource")));
+                        BookListBox.ItemsSource = bookNameList;
+                    }
+                    break;
+                case "Edit" :
+                    BookForm bookformwin = new BookForm(bookhandle.getBookByName(BookListBox.SelectedItem.ToString()));
+                    bookformwin.Show();
 
-            //Loading object into the DataView
-            var bookHander = new BookHandler();
-            bookModelViewSource.Source = bookHander.getBooks();
-             * */
+                    //Deleting old bookname and readding it due to ObservableCollection not picking up changes
+                    var updatedBook = bookHander.getBookByName(BookListBox.SelectedValue.ToString());
+                    bookNameList.RemoveAt(BookListBox.SelectedIndex);
+                    bookNameList.Add(updatedBook.Title);
+
+                    break;
+                case "Add Book" :
+                    break;
+            }
         }
     }
 }
